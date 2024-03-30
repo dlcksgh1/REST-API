06b07a0fa2289d56688adbee245305dc28c0ced5
Lee  Chanho <sma05091@nate.com>
2024-03-30 10:26:28 +0900

    Event 생성 API -Bad Request 처리하기 1. DTO에 정의된 값 이외의 것을 보내는 것 에 대한 Bad Request 처리

--- src/test/java/me/chanho/restapi/events/EventControllerTests.java
+++ src/test/java/me/chanho/restapi/events/EventControllerTests.java
@@ -33,6 +33,37 @@ public class EventControllerTests {
     @Test
     public void createEvent() throws Exception {
 
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEventDateTime(LocalDateTime.of(2024, 03, 24, 11, 45))
+                .endEventDateTime(LocalDateTime.of(2024, 03, 25, 11, 45))
+                .beginEnrollmentDateTime(LocalDateTime.of(2024, 03, 25, 11, 45))
+                .closeEnrollmentDateTime(LocalDateTime.of(2024, 03, 26, 11, 45))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE+";charset=UTF-8"))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+
+    }
+
+    @Test
+    public void createEvent_BadRequest() throws Exception {
+
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -55,13 +86,9 @@ public class EventControllerTests {
                         .accept(MediaTypes.HAL_JSON)
                         .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE+";charset=UTF-8"))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                .andExpect(status().isBadRequest());
+
 
     }
+
 }
